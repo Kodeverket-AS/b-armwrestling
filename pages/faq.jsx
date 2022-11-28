1dc2e3d551cf4323a584a30849d2a4b3991299eb
Ole Håvard Furuseth Bergan <oleh.kodehode@gmail.com>
2022-11-28 13:43:06 +0100

    test

---
+++ pages/faq.jsx
@@ -0,0 +1,15 @@
+const Faq = () => {
+    return(
+        <>
+            <h1 className="text-5xl text-center text-yellow-500">Spørsmål og svar</h1>
+            <div className=" accordion">
+                <div className=" accordion">
+
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Faq;

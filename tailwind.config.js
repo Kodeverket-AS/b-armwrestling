ba4bdb3c36fada8c0b03b60c45c973c28c710dc6
jonfossheim <fossheimjoneven@gmail.com>
2022-11-28 14:53:00 +0100

    feat: added flowbite-react

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './node_modules/flowbite-react/**/*.js',
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
-    "./node_modules/flowbite/**/*.{js,ts,jsx,tsx}"
+    './node_modules/flowbite/**/*.{js,ts,jsx,tsx}',
   ],
+  plugins: [require('flowbite/plugin')],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
-}
+};

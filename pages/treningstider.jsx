3ca75eced74f9ea1ddaa103d3a48b221bcdea76d
Ole Håvard Furuseth Bergan <oleh.kodehode@gmail.com>
2022-11-29 16:12:08 +0100

    small bug fixes treningstider

--- pages/treningstider.jsx
+++ pages/treningstider.jsx
@@ -12,7 +12,7 @@ const Treningstider = () => {
                 className="grid justify-center grid-cols-1 text-white md:grid-cols-2 md:gap-12"
             >
                 <div
-                    className="grid h-24 md:grid-rows-2 md:justify-end md:h-48"
+                    className="grid md:grid-rows-2 md:justify-end md:h-48"
                 >
                     <div
                         className="grid grid-cols-2 gap-4 text-2xl"
@@ -29,20 +29,26 @@ const Treningstider = () => {
                             19.00-21.00
                         </p>
                     </div>
-                    <div>
-                        <p
-                            className="text-center w-96"
-                        >
-                            Følg oss på
+                    <div
+                        // className="grid grid-rows-2 gap-16 mx-8 w-96"
+                        // className="grid h-64 grid-rows-2"
+                        className="mx-10"
+                    >
+                        <p>
+                            Fortløpende oppdatert informasjon om trening, arrangementer med mer legges ut på vår lukkete
                             {' '}
                             <Link
                                 className='underline hover:text-ba-color-gold'
                                 href="https://www.facebook.com/profile.php?id=100063867946394"
                             >
-                                facebook
+                                Facebook gruppe
                             </Link>
                             {' '}
-                            for den meste oppdaterte infomasjonen
+                            for våre medlemmer.
+                        </p>
+                        <br/>
+                        <p>
+                        Vi trener fast hver tirsdag på Gnisten Aktivitetssenter. Vi låner dette lokalet av Bergen Kommune og ønsker et tett sammarbeid med dem for å fremme klubben og idretten vår.
                         </p>
                     </div>
                 </div>
@@ -50,7 +56,7 @@ const Treningstider = () => {
                     className="grid grid-cols-1 my-8 text-center"
                 >
                     <iframe
-                        className="mx-auto my-6 md:w-80 md:h-64 md:ml-0 lg:w-ba-width-large"
+                        className="mx-auto my-6 md:w-80 md:h-64 md:ml-0 xl:w-ba-width-large"
                         src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA-eLtEdtB-XioubLC_9yUcxUja3xYrukI&q=Gnisten_Aktivitetssenter,Bergen"
                     />
                 </div>

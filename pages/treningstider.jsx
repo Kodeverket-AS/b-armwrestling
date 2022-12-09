54dc1b599e36fcdab43a30b3063f15e266583cce
Andreastak <104064121+Andreastak86@users.noreply.github.com>
2022-12-09 15:09:14 +0100

    changes

--- pages/treningstider.jsx
+++ pages/treningstider.jsx
@@ -17,9 +17,9 @@ const Treningstider = () => {
             <h1 className='pt-8 mb-8 text-5xl text-center text-ba-color-gold '>
                 Treningstider
             </h1>
-            <section className='px-4 '>
-                <div className='flex flex-row content-between text-white gap-9 '>
-                    <p className='w-3/5 '>
+            <section className='mx-20 '>
+                <div className='grid grid-cols-2 gap-10 text-white '>
+                    <p className='border-2 border-red-500'>
                         <span className='text-lg font-bold leading-10 text-white'>
                             Treningstider: 19.00 - 21.00
                         </span>
@@ -36,17 +36,22 @@ const Treningstider = () => {
                         Bergen Kommune og ønsker et tett sammarbeid med dem for
                         å fremme klubben og idretten vår.
                     </p>
-                    <Image
-                        className='w-2/5 '
-                        src={inngang_narrow}
-                        alt='Inngang sett fra parkeringen'
-                    ></Image>
+                    <div className='border-2 border-blue-600'>
+                        <Image
+                            width={600}
+                            src={inngang_narrow}
+                            alt='Inngang sett fra parkeringen'
+                        />
+                        <p className='text-xs '>
+                            Inngang sett fra parkeringsplass
+                        </p>
+                    </div>
                     {/* <p className='text-xs'>Inngang sett fra parkeringen</p> */}
                 </div>
+
                 <div>
                     <iframe
-                        // xl:w-ba-width-large
-                        className='mx-auto my-4 w-[100%] h-[350px] py-6  '
+                        className='mx-auto my-4 w-[100%] h-[450px] py-6  '
                         src='https://www.google.com/maps/embed/v1/place?key=AIzaSyA-eLtEdtB-XioubLC_9yUcxUja3xYrukI&q=Gnisten_Aktivitetssenter,Bergen'
                     />
                 </div>

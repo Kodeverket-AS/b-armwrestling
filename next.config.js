18b084e285a35d0c17f608859e662750cd017c3f
Ole Håvard Furuseth Bergan <oleh.kodehode@gmail.com>
2022-12-12 12:55:32 +0100

    added i18

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
+const { i18n } = require('./next-i18next.config')
+
 module.exports = {
   reactStrictMode: true,
-  i18n: {
-    locales: ['eng', 'nor'],
-    defaultLocale: 'no',
-  },
+  i18n,
 }

581edccb0d3b7a77102045b5ac1eec41f750d36c
JamesDa1 <104003738+JamesDa1@users.noreply.github.com>
2022-12-13 16:31:40 +0100

    added ”” to links

--- pages/terminliste.jsx
+++ pages/terminliste.jsx
@@ -9,34 +9,34 @@ import { BsFillCalendarCheckFill } from "react-icons/bs"
 
 import { AiOutlineArrowRight } from "react-icons/ai"
 
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { serverSideTranslations } from "next-i18next/serverSideTranslations"
 import { useTranslation } from "react-i18next"
 
 export async function getStaticProps({ locale }) {
   return {
     props: {
       ...(await serverSideTranslations(locale, [
-        'common',
-        'hero',
-        'schedule',
-        'termlist',
+        "common",
+        "hero",
+        "schedule",
+        "termlist",
       ])),
     },
   }
 }
 const TermList = () => {
-  const { t } = useTranslation('termlist')
+  const { t } = useTranslation("termlist")
   return (
     <>
       <Head>
-        <title>{t('Title')}</title>
+        <title>{t("Title")}</title>
         <meta
           name="description"
           content="Terminliste for Bergen Armwrestling Club, vi konkurerer lokalt og internasjonalt. Påmelding skjer gjennom deltaker.no"
         ></meta>
       </Head>
       <h1 className="pt-8 mb-8 text-5xl text-center text-ba-color-gold">
-        {t('Title')}
+        {t("Title")}
       </h1>
       {/* Events container  */}
 
@@ -46,20 +46,20 @@ const TermList = () => {
             <Timeline.Point icon={BsFillCalendarCheckFill} />
             <Timeline.Content>
               <Timeline.Time className="text-base italic text-white">
-                {t('Hardanger date')}
+                {t("Hardanger date")}
               </Timeline.Time>
               <Timeline.Title className="font-bold text-ba-color-gold ">
-                {t('Hardanger')}
+                {t("Hardanger")}
               </Timeline.Title>
               <Timeline.Body className="text-white">
-                {t('Info championship')}
+                {t("Info championship")}
               </Timeline.Body>
               <Button
                 color="gray"
                 className="text-white bg-ba-color-gold transition duration-500 border-0 hover:bg-white hover:text-black hover:scale-105 md:h-full h-[2rem]"
               >
                 <Link href="#" target="_blank">
-                  {t('Read more')}
+                  {t("Read more")}
                 </Link>
                 <AiOutlineArrowRight className="w-3 h-3 ml-2" />
               </Button>
@@ -69,20 +69,20 @@ const TermList = () => {
             <Timeline.Point icon={BsFillCalendarCheckFill} />
             <Timeline.Content>
               <Timeline.Time className="text-base italic text-white">
-                {t('Romania time')}
+                {t("Romania time")}
               </Timeline.Time>
               <Timeline.Title className="font-bold text-ba-color-gold ">
-                {t('Romania')}
+                {t("Romania")}
               </Timeline.Title>
               <Timeline.Body className="text-white">
-                {t('Info championship')}
+                {t("Info championship")}
               </Timeline.Body>
               <Button
                 color="gray"
                 className="text-white bg-ba-color-gold transition duration-500 border-0 hover:bg-white hover:text-black hover:scale-105 md:h-full h-[2rem]"
               >
                 <Link href="#" target="_blank">
-                  {t('Read more')}
+                  {t("Read more")}
                 </Link>
                 <AiOutlineArrowRight className="w-3 h-3 ml-2" />
               </Button>
@@ -92,20 +92,20 @@ const TermList = () => {
             <Timeline.Point icon={BsFillCalendarCheckFill} />
             <Timeline.Content>
               <Timeline.Time className="text-base italic text-white">
-                {t('Skien time')}
+                {t("Skien time")}
               </Timeline.Time>
               <Timeline.Title className="font-bold text-ba-color-gold ">
-                {t('Skien')}
+                {t("Skien")}
               </Timeline.Title>
               <Timeline.Body className="text-white">
-                {t('Info championship')}
+                {t("Info championship")}
               </Timeline.Body>
               <Button
                 color="gray"
                 className="text-white bg-ba-color-gold transition duration-500 border-0 hover:bg-white hover:text-black hover:scale-105 md:h-full h-[2rem]"
               >
                 <Link href="#" target="_blank">
-                  {t('Read more')}
+                  {t("Read more")}
                 </Link>
                 <AiOutlineArrowRight className="w-3 h-3 ml-2" />
               </Button>
@@ -115,20 +115,20 @@ const TermList = () => {
             <Timeline.Point icon={BsFillCalendarCheckFill} />
             <Timeline.Content>
               <Timeline.Time className="text-base italic text-white">
-                {t('Sandefjord time')}
+                {t("Sandefjord time")}
               </Timeline.Time>
               <Timeline.Title className="font-bold text-ba-color-gold ">
-                {t('Sandefjord')}
+                {t("Sandefjord")}
               </Timeline.Title>
               <Timeline.Body className="text-white">
-                {t('Info championship')}
+                {t("Info championship")}
               </Timeline.Body>
               <Button
                 color="gray"
                 className="text-white bg-ba-color-gold transition duration-500 border-0 hover:bg-white hover:text-black hover:scale-105 md:h-full h-[2rem]"
               >
                 <Link href="#" target="_blank">
-                  {t('Read more')}
+                  {t("Read more")}
                 </Link>
                 <AiOutlineArrowRight className="w-3 h-3 ml-2" />
               </Button>
@@ -138,20 +138,20 @@ const TermList = () => {
             <Timeline.Point icon={BsFillCalendarCheckFill} />
             <Timeline.Content>
               <Timeline.Time className="text-base italic text-white">
-                {t('Turkey time')}
+                {t("Turkey time")}
               </Timeline.Time>
               <Timeline.Title className="font-bold text-ba-color-gold ">
-                {t('Turkey')}
+                {t("Turkey")}
               </Timeline.Title>
               <Timeline.Body className="text-white">
-                {t('Info championship')}
+                {t("Info championship")}
               </Timeline.Body>
               <Button
                 color="gray"
                 className="text-white bg-ba-color-gold transition duration-500 border-0 hover:bg-white hover:text-black hover:scale-105 md:h-full h-[2rem]"
               >
                 <Link href="#" target="_blank">
-                  {t('Read more')}
+                  {t("Read more")}
                 </Link>
                 <AiOutlineArrowRight className="w-3 h-3 ml-2" />
               </Button>
@@ -161,20 +161,20 @@ const TermList = () => {
             <Timeline.Point icon={BsFillCalendarCheckFill} />
             <Timeline.Content>
               <Timeline.Time className="text-base italic text-white">
-                {t('Trondheim time')}
+                {t("Trondheim time")}
               </Timeline.Time>
               <Timeline.Title className="font-bold text-ba-color-gold ">
-                {t('Trondheim')}
+                {t("Trondheim")}
               </Timeline.Title>
               <Timeline.Body className="text-white">
-                {t('Info championship')}
+                {t("Info championship")}
               </Timeline.Body>
               <Button
                 color="gray"
                 className="text-white bg-ba-color-gold transition duration-500 border-0 hover:bg-white hover:text-black hover:scale-105 md:h-full h-[2rem]"
               >
                 <Link href="#" target="_blank">
-                  {t('Read more')}
+                  {t("Read more")}
                 </Link>
                 <AiOutlineArrowRight className="w-3 h-3 ml-2" />
               </Button>
@@ -184,20 +184,20 @@ const TermList = () => {
             <Timeline.Point icon={BsFillCalendarCheckFill} />
             <Timeline.Content>
               <Timeline.Time className="text-base italic text-white">
-                {t('Romania time')}
+                {t("Romania time")}
               </Timeline.Time>
               <Timeline.Title className="font-bold text-ba-color-gold ">
-                {t('Romania')}
+                {t("Romania")}
               </Timeline.Title>
               <Timeline.Body className="text-white">
-                {t('Info championship')}
+                {t("Info championship")}
               </Timeline.Body>
               <Button
                 color="gray"
                 className="text-white bg-ba-color-gold transition duration-500 border-0 hover:bg-white hover:text-black hover:scale-105 md:h-full h-[2rem]"
               >
                 <Link href="#" target="_blank">
-                  {t('Read more')}
+                  {t("Read more")}
                 </Link>
                 <AiOutlineArrowRight className="w-3 h-3 ml-2" />
               </Button>
@@ -207,16 +207,16 @@ const TermList = () => {
       </div>
       {/* Disclaimer */}
       <p className="px-4 py-4 mx-auto text-white w-[95%] max-w-[650px] mb-8">
-        {t('Body text')}{" "}
+        {t("Body text")}{" "}
         <Link
           href="https://www.deltaker.no"
           target="_blank"
-          title={t('Link des')}
+          title={t("Link des")}
           className="text-white underline hover:text-ba-color-gold"
         >
-          deltaker.no
+          ”deltaker.no”
         </Link>{" "}
-        {t('Body text part 2')}
+        {t("Body text part 2")}
       </p>
     </>
   )

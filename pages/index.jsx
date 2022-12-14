181bdc40d5b2039a919e6a6ac6d732ce43254c14
Andreastak <104064121+Andreastak86@users.noreply.github.com>
2022-12-14 11:40:46 +0100

    try changes on pics and text

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,29 +1,27 @@
-import Hero from '../components/Hero';
+import Hero from "../components/Hero";
 
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 export async function getStaticProps({ locale }) {
-  return {
-    props: {
-      ...(await serverSideTranslations(locale, [
-        'common',
-        'hero',
-        'schedule',
-        'faq,',
-        'termlist'
-      ])),
-    },
-  }
+    return {
+        props: {
+            ...(await serverSideTranslations(locale, [
+                "common",
+                "hero",
+                "schedule",
+                "faq,",
+                "termlist",
+            ])),
+        },
+    };
 }
 
-
-const Home = ({locale}) => {
-  return (
-    <>
-      <Hero />
-    </>
-  );
+const Home = ({ locale }) => {
+    return (
+        <>
+            <Hero />
+        </>
+    );
 };
 
 export default Home;

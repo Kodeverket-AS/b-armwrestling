31727bc483c068b896ef6ace87aefd3d31cb6f45
JamesDa1 <husniadan@gmail.com>
2022-12-15 15:05:01 +0100

    added norwegian

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -6,6 +6,8 @@ import { MdArrowDropDown } from "react-icons/md"
 import { useTranslation } from "next-i18next"
 import { useRouter } from "next/router"
 
+import flag_norwegian from "../public/flag_norway.svg"
+
 const NavBar = () => {
   const { t } = useTranslation("common")
 
@@ -129,6 +131,7 @@ const NavBar = () => {
         </div>
         <div className="text-center text-white ">
           <button
+            className="flex items-center justify-center"
             onClick={() => {
               if (router.locale == "en") {
                 router.push(`/no${router.route}`)
@@ -137,9 +140,12 @@ const NavBar = () => {
               }
             }}
           >
-            {router.locale === "no"
-              ? "Change to English"
-              : "Bytt til Norsk"}
+            {router.locale === "no" ? `Change to English` : "Bytt til Norsk"}
+            {router.locale === "en" ? (
+              <Image src={flag_norwegian} />
+            ) : (
+              <Image src={flag_norwegian} />
+            )}
           </button>
         </div>
       </div>
